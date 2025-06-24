a620d6ddcf1dcea5882b16beea1cdf9b3ce744ed
Petrunyandr <andrei.petrun07@gmail.com>
2025-06-24 19:08:34 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,7 +1,8 @@
+import asyncio
 import telebot as t
 from telebot import types
 
-bot = t.TeleBot('7975402209:AAGilNMkPgXsoevUdWb-ZCovt2vOtPS9vGs')
+bot = t.AsyncTeleBot('7975402209:AAGilNMkPgXsoevUdWb-ZCovt2vOtPS9vGs')
 
 bot.set_my_commands([
 types.BotCommand("/start",
@@ -12,24 +13,33 @@ types.BotCommand("/music", "послушать музыку ")
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
+async def start(message):
+	await
     bot.send_message(message.from_user.id, "пр")
 #сори
 
 @bot.message_handler(func=lambda
-message: message.text.lower() == "ку")
-def ku(message):
+message: message.text and message.text.lower() == "ку")
+async def ku(message):
+	await
 	bot.send_message(message.chat.id, "нет")
 
 
 @bot.message_handler(commands=['music'])
-def music(message):
+async def music(message):
+	await 
 	bot.send_message(message.from_user.id, "подождите пару секунд")
 	voice = open('/storage/emulated/0/Download/zweielephanten', 'rb')
+	await
 	bot.send_audio(message.chat.id,
 	voice,
 	title="Zwei elefanten",
 	performer="Наталия Владимировна")
 	voice.close()
+async def main():
+    await bot.set_webhook()  # если используешь webhook, иначе можно убрать
+    await bot.polling()
 
+if __name__ == '__main__':
+    asyncio.run(main())
 bot.polling(none_stop=True)
